58aaab83b3df612f69a6c18d77b9d273209bf595
martin-coder <martincoder1@gmail.com>
2019-05-05 19:30:50 -0400

    Reworked Card class so it now keeps both its value and suit as ints, and uses them to create strings using the two String arrays containing all possible values and suits when the toString method is called.
    Fixed remove method of Deck class, now actually returns Card object that was removed.
    
    Added getNumCards accessor method to Deck class

--- src/Deck.java
+++ src/Deck.java
@@ -3,57 +3,64 @@ import java.util.Random;
 
 public class Deck {
 
-    private class Card {
+    public class Card {
 
         private int value;
-        private String suit;
+        private int suit;
 
-        private int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        private String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        private String[] values = {null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        private String[] suits = {null, "Hearts", "Diamonds", "Clubs", "Spades"};
 
         private Card(int value, int suit) {
             this.value = value;
-            this.suit = suits[suit];
+            this.suit = suit;
         }
 
         public String toString() {
-            return this.value + this.suit;
+            return values[this.value] + " of " + suits[this.suit];
         }
     }
 
     private ArrayList<Card> deck;
     private int numCards;
 
-    public Deck() {
+    Deck(boolean empty) {
         this.numCards = 0;
         this.deck = new ArrayList<>(52);   //  All decks have max capacity of 52, so there is no need to mess around with resizing
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 12; j++) {
-                Card card = new Card(j, i);
-                deck.add(card);
-                numCards++;
+        if (!empty) {
+            for (int i = 1; i < 5; i++) {
+                for (int j = 1; j < 14; j++) {
+                    Card card = new Card(j, i);
+                    deck.add(card);
+                }
             }
         }
     }
 
-    public Deck(ArrayList<Card> cards) {
+    Deck(ArrayList<Card> cards) {
         this.numCards = 0;
         this.deck = new ArrayList<>(52);
 
         deck.addAll(cards);
     }
 
-    public void addCard(Card card) {
+    int getNumCards() {
+        return deck.size();
+    }
+
+    void addCard(Card card) {
         deck.add(card);
         numCards++;
     }
 
-    public Card removeCard() {
-        return deck.remove(deck.size()-1);
+    Card removeCard() {
+        Card removed = deck.get(deck.size()-1);
+        deck.remove(deck.size()-1);
+        return removed;
     }
 
-    public void shuffle() {
+    void shuffle() {
         Random r = new Random();
 
         for (int i = 0; i < deck.size(); i++) {
@@ -70,4 +77,5 @@ public class Deck {
     public String toString() {
         return deck.toString();
     }
+
 }
